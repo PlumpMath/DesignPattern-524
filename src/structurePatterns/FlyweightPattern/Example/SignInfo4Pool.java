251d37cf64bc9425e69c4f498c8bb3cc30d39427
wuzhiyu <name.wuzhiyu@gmail.com>
2014-10-20 23:50:30 +0800

    add an example of flyweight pattern.

---
+++ src/structurePatterns/FlyweightPattern/Example/SignInfo4Pool.java
@@ -0,0 +1,17 @@
+package structurePatterns.FlyweightPattern.Example;
+
+class SignInfo4Pool extends SignInfo {
+	private String key;
+	
+	public SignInfo4Pool(String key) {
+		this.key=key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+}
---
+++ src/structurePatterns/FlyweightPattern/Example/SignInfoFactory.java
@@ -0,0 +1,24 @@
+package structurePatterns.FlyweightPattern.Example;
+
+import java.util.HashMap;
+
+class SignInfoFactory {
+	private static HashMap<String, SignInfo> pool=new HashMap<String, SignInfo>();
+	@Deprecated
+	public static SignInfo getSignInfo(){
+		return new SignInfo();
+	}
+	
+	public static SignInfo getSignInfo(String key){
+		SignInfo result=null;
+		if(!pool.containsKey(key)){
+			System.out.println(key+"----建立对象，并放置到池中");
+			result=new SignInfo4Pool(key);
+			pool.put(key, result);
+		}else{
+			result=pool.get(key);
+			System.out.println(key+"----直接从池中获得");
+		}
+		return result;
+	}
+}
